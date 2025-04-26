a5da5568590330b866711f4e2cdea50c01aa6f7c
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-26 12:19:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var yt = document.getElementById("youtube");
+var dc = document.getElementById("discord");
+var gh = document.getElementById("github");
+
+yt.addEventListener("click", function() {
+  window.location.href='https://youtube.com/@ruikurenaii';
+});
+dc.addEventListener("click", function() {
+  window.location.href='https://discord.gg/3yHUfvPkCw';
+});
+gh.addEventListener("click", function() {
+  window.location.href='https://github.com/ruikurenaii';
+});
